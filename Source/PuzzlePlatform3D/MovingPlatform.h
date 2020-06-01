a7de1aacf79173dbbf2bde59087a62c0cf9d7055
Alejandro Saucedo <axsauze@gmail.com>
2020-06-01 07:32:49 +0100

    Added functionaity to connect netwrok and sync moving platform

---
+++ Source/PuzzlePlatform3D/MovingPlatform.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORM3D_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	float PlatformMoveSpeed = 20;
+
+	UPROPERTY(EditAnywhere, Meta=(MakeEditWidget=true))
+	FVector TargetLocation;
+};
