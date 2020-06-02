b3113e729066d70c1bca56ba0a9793b6c3a64cd5
Alejandro Saucedo <axsauze@gmail.com>
2020-06-02 18:41:53 +0100

    Adde initial basis to show the menu, now separate in a different level

--- Source/PuzzlePlatform3D/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatform3D/PuzzlePlatformsGameInstance.cpp
@@ -4,15 +4,26 @@
 #include "PuzzlePlatformsGameInstance.h"
 
 #include "Engine/Engine.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+
+#include "PlatformTrigger.h"
 
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer &ObjectInitializer)
 {
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<UUserWidget> MenuBPClass(TEXT("/Game/MenuSystem/WBP_MainMenu"));
+	if (!ensure(MenuBPClass.Class != nullptr)) return;
+
+	MenuClass = MenuBPClass.Class;
+
 	UE_LOG(LogTemp, Warning, TEXT("Game instance constructor"));
 }
 
 void UPuzzlePlatformsGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Game instance Init"));
+	UE_LOG(LogTemp, Warning, TEXT("Found Class %s"), *MenuClass->GetName());
 }
 
 void UPuzzlePlatformsGameInstance::Host()
@@ -41,4 +52,14 @@ void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Now running %s"), *Address));
 	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joined %s"), *Address));
+}
+
+void UPuzzlePlatformsGameInstance::LoadMenu()
+{
+	if (!ensure(MenuClass != nullptr)) return;
+
+	UUserWidget* Menu = CreateWidget<UUserWidget>(this, MenuClass);
+	if (!ensure(Menu != nullptr)) return;
+
+	Menu->AddToViewport();
 }
--- Source/PuzzlePlatform3D/PuzzlePlatformsGameInstance.h
+++ Source/PuzzlePlatform3D/PuzzlePlatformsGameInstance.h
@@ -19,9 +19,15 @@ public:
 
 	virtual void Init();
 
+	UFUNCTION(BlueprintCallable)
+	void LoadMenu();
+
 	UFUNCTION(Exec)
 	void Host();
 
 	UFUNCTION(Exec)
 	void Join(const FString& Address);
+
+private:
+	TSubclassOf<class UUserWidget> MenuClass;
 };
